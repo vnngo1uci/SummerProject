0727c9ed62b2af23ec931ba3014d446b1548bf71
vnngo1uci <ngovince9@gmail.com>
2023-06-18 17:10:03 -0700

    test code

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-import pandas as pd
+import openpxyl
 
-df = pd.read_excel(r'Path where the Excel file is stored\File name.xlsx')
-print(df)
+data_file = '/Users/vincentngo/Desktop/Summer23/SummerProject/ABCDCatering.xls'
+
+workbook_obj = openpxyl.load_workbook(data_file)
+
+sheet_obj = workbook_obj.active
+ 
+
+cell_obj = sheet_obj.cell(row = 1, column = 1)
+ 
+
+print(cell_obj.value)
